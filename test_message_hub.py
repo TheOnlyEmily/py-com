f58591718fd8f33adca69136dc3fc3b7e0e74993
Emily Ingram <emingram49@gmail.com>
2020-05-23 15:25:27 -0600

    Update test_message_hub.py

--- test_message_hub.py
+++ test_message_hub.py
@@ -7,6 +7,7 @@ def test_init():
 
     assert mh._reciever_table == {"HELLO":[], "GOODBYE":[]}
 
+
 class TestSendMessage():
 
     def test_two_recievers_two_messages(self):
@@ -39,3 +40,29 @@ class TestSendMessage():
 
         assert mr1.status == "recieved: HELLO WORLD"
         assert mr2.status == "recieved: GOODBYE EVENTS"
+
+    def test_two_recievers_one_message(self):
+        POSSIBLE_MESSAGES = ["HELLO"]
+        mh = MessageHub(POSSIBLE_MESSAGES)
+
+        class MyReciever:
+
+            def __init__(self):
+                self.status = "waiting"
+
+            def recieve_method(self, message, x):
+                self.status = "recieved: {} {}".format(message, x)
+
+        mr1 = MyReciever()
+        mr2 = MyReciever()
+
+        mh.add_reciever(mr1.recieve_method, "HELLO")
+        mh.add_reciever(mr2.recieve_method, "HELLO")
+
+        assert mr1.status == "waiting"
+        assert mr2.status == "waiting"
+
+        mh.send_message("HELLO", "WORLD")
+
+        assert mr1.status == "recieved: HELLO WORLD"
+        assert mr2.status == "recieved: HELLO WORLD"
