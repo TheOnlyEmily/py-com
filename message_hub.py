668c7a0f2707b5ba0fb99d3981af9487c655ad1e
Emily Ingram <emingram49@gmail.com>
2020-05-23 21:28:34 -0600

    Update message_hub.py

--- message_hub.py
+++ message_hub.py
@@ -36,6 +36,9 @@ class MessageHub:
 
     def __init__(self, acceptable_messages):
         """
+        Creates a new message hub, by specifying a list of valid
+        message types.
+
         Args:
             acceptable_messages (tuple[T] | list[T]): A sequence of acceptable
             messages that can be sent/recieved.
@@ -43,10 +46,37 @@ class MessageHub:
         self._reciever_table = {m:[] for m in acceptable_messages}
 
     def add_reciever(self, rec_func, message):
+        """
+        Adds a function that can recieve a type of message message.
+
+        Args:
+            rec_func (function | method): The function that the message and
+            its contents will be passed to.
+            message (T): The message type that the rec_func recieves.
+
+        Raises:
+            KeyError: If message is not in the reciever_table, and is
+            not a valid message type.
+        """
         self._reciever_table[message].append(rec_func)
 
-    def send_message(self, message_name, *message_info):
-        recievers = self._reciever_table[message_name]
-        full_message = (message_name,) + message_info
+    def send_message(self, message, *message_info):
+        """
+        Sends a message and the info it comes with, to the function(s)
+        or method(s) that are meant to recieve them.
+
+        Args:
+            message (T): The message that will contain message_into.
+            message_info (tuple[T]): Optional, additional info that
+            will be passed with the message.
+
+        Raises:
+            KeyError: When message is not a valid message type.
+            TypeError: When the length message, combined with the
+            message_info, is greater than the number of arguments the
+            function/method recieveing them can accomidate.
+        """
+        recievers = self._reciever_table[message]
+        full_message = (message,) + message_info
         for r in recievers:
             r(*full_message)
