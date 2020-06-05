51c00fb21d031b7c21edd651ddb356e59ad40b11
Emily Ingram <emingram49@gmail.com>
2020-06-04 21:24:14 -0600

    update cmd_line_calc.py

--- examples/cmd_line_calc.py
+++ examples/cmd_line_calc.py
@@ -2,27 +2,29 @@ from context import py_com
 from py_com.message_hub import MessageHub
 
 
-calc_hub = MessageHub(["GET_INPUT", "INPUT", "ANSWER"])
+calc_hub = MessageHub(["INPUT", "ANSWER", "ERROR"])
 
-def get_cli_input(message):
+def get_input():
     input_equation = input("Enter equation: ")
-    calc_hub.send_message("INPUT", input_equation)
+    MessageHub.get_instance().send_message("INPUT", input_equation)
 
 def eval_result(message, equation):
+    m_hub = MessageHub.get_instance()
     try:
         result = eval(equation)
+        m_hub.send_message("ANSWER", result)
     except SyntaxError:
-        raise SystemExit()
-    calc_hub.send_message("ANSWER", result)
+        m_hub.send_message("ERROR", equation)
 
-def cli_display_answer(message, answer):
+def display_answer(message, answer):
     print("The answer is: ", answer)
-    input("Press any key to continue.")
 
-calc_hub.add_reciever(get_cli_input, "GET_INPUT")
+def display_error(message, equation):
+    print("Error! {} is not valid python!".format(equation))
+
 calc_hub.add_reciever(eval_result, "INPUT")
-calc_hub.add_reciever(cli_display_answer, "ANSWER")
+calc_hub.add_reciever(display_answer, "ANSWER")
+calc_hub.add_reciever(display_error, "ERROR")
 
 if __name__ == "__main__":
-    while True:
-        calc_hub.send_message("GET_INPUT")
+    get_input()
